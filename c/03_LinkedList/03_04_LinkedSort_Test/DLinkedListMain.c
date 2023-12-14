846d21a085527c119adafc802c8ec1e805446348
choadd <choadd@naver.com>
2023-12-14 17:13:33 +0000

    linked list set sort rule

---
+++ c/03_LinkedList/03_04_LinkedSort_Test/DLinkedListMain.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "DLinkedList.h"
+
+int WhoIsPrecede(int d1, int d2){
+    if(d1 < d2)
+        return 0;
+    else   
+        return 1;
+}
+
+int main(void){
+    List list;
+    int data;
+    ListInit(&list);
+
+    SetSortRule(&list, WhoIsPrecede);
+
+    LInsert(&list, 8);
+    LInsert(&list, 5);
+    LInsert(&list, 2);
+    LInsert(&list, 9);
+
+    printf("data count : %d\n", LCount(&list));
+
+    if(LFirst(&list, &data)){
+        printf("first : %d\n", data);
+        while(LNext(&list, &data)){
+            printf("next : %d\n", data);            
+        }
+    }
+
+}
